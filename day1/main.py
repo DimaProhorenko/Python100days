d29994e82219bd5cac6f3d843e3d4cf4ca1179a3
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-09-18 10:17:15 +0300

    Day 1

---
+++ day1/main.py
@@ -0,0 +1,8 @@
+# print("Hello " + "Dima " + "you are awesome")
+# print("Day 1 - String Manipulation")
+# print("String Concatenation is done with the " + '"+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+name = input("Enter your name: ")
+print("Hello " + name)
