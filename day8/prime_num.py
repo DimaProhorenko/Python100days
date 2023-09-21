712c51f8f7472e9c88a9a3027ac6b6e05f349e40
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-09-22 00:00:42 +0300

    Start day 8

---
+++ day8/prime_num.py
@@ -0,0 +1,10 @@
+def prime_checker(number):
+    for i in range(2, number):
+        if number % i == 0:
+            print("It's not a prime number.")
+            return
+    print("It's a prime number.")
+
+
+n = int(input("Check this number: "))
+prime_checker(number=n)
