577c41aaa914f66da0ddbb1f2a484cf8b40ecbf2
Dmitriy Prokhorenko <dima.study.web@gmail.com>
2023-09-23 22:50:48 +0300

    FINISH day 8

--- day8/caesar_cipher.py
+++ day8/caesar_cipher.py
@@ -1,10 +1,37 @@
 
+def caesar(msg, shift, mode="encode"):
+    ascii = convert_to_ascii(msg)
+    result = []
+    for n in ascii:
+        if n != 32:
+            if mode == "encode":
+                result.append(chr(n + shift))
+            else:
+                result.append(chr(n - shift))
+        else:
+            result.append(chr(n))
+
+    return "".join(result)
+
+
 def encode(msg, shift):
     ascii = convert_to_ascii(msg)
     result = []
     for n in ascii:
         if n != 32:
             result.append(chr(n + shift))
+        else:
+            result.append(chr(n))
+
+    return "".join(result)
+
+
+def decode(msg, shift):
+    ascii = convert_to_ascii(msg)
+    result = []
+    for n in ascii:
+        if n != 32:
+            result.append(chr(n - shift))
         else:
             result.append(chr(n))
 
@@ -18,7 +45,16 @@ def convert_to_ascii(msg):
     return ascii_list
 
 
-message = input("Enter your message: ")
-shift_num = int(input("Enter the shift number: "))
+run = True
 
-print(encode(message, shift_num))
+while run:
+    keep_playing = input("Type 'c' to continue, 'e' to exit: ")
+    if keep_playing == "e":
+        print("Finished")
+        break
+    else:
+        mode = input("Type 'encode' to encrypt, 'decode' to decrypt: ").lower()
+        message = input("Enter your message: ")
+        shift_num = int(input("Enter the shift number: "))
+
+        print(caesar(message, shift_num, mode))
